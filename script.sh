4c1485b4f4a4f6991a8be837ef382a655235b0a7
hramykalu <90501314+hramykalu@users.noreply.github.com>
2022-01-14 19:35:39 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+cd mainfolder ;
+mkdir folder1 folder2 folder3 ;
+cd folder1 ;
+touch file1.txt file2.txt file3.txt file4.json file5.json ;
+mkdir folder_a folder_b folder_c ;
+ls -la; 
+mv file2.txt file3.txt folder_a ;
